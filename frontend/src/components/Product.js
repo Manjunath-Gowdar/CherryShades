1b1502ae713c9f2664a16f0dea2b001108a01a5c
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2021-10-25 11:03:27 +0530

    Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -9,17 +10,19 @@ const Product = ({ product }) => {
       </a>
 
       <Card.Body>
-      <a href={`/product/${product._id}`}>
-        <Card.Title as='div'>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>
             <strong>{product.name}</strong>
-        </Card.Title>
-      </a>
+          </Card.Title>
+        </a>
 
-      <Card.Text>
-          <div className='my-3'>
-              {product.rating} from {product.numReviews} reviews 
-          </div>
-      </Card.Text>
+        <Card.Text>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          
+          />
+        </Card.Text>
 
         <Card.Text as='h3'>₹{product.price}</Card.Text>
       </Card.Body>
