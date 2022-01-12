e82a6298ef7c2ce16f22cf32e88ce81547a05de6
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-01-12 17:04:41 +0530

    Converting server.js file from common.js to es6module,(i.e ES Modules in Node.js)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 dotenv.config()
-const products = require('./data/products')
+
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running')
