57201b3e1074d2ddd2da821b146743f3defb6c26
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-01-12 10:48:00 +0530

    dotenv package setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
 
+dotenv.config()
 const products = require('./data/products')
 
-app.get('/',(req,res)=>{
-    res.send("API is running")
+app.get('/', (req, res) => {
+  res.send('API is running')
 })
-
 
 app.get('/api/products', (req, res) => {
   res.json(products)
@@ -17,4 +18,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('server is running'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`server is in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
