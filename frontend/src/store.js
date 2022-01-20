66e0cd66407506b02c358b1a8a84175664b6a137
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-01-20 23:03:20 +0530

    Added comment to store.js file

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,16 @@ import {createStore,combineReducers,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
+// to combine all the middle wares here
 const reducers = combineReducers({})
 
+// you can add any data for initialState 
 const initialState = {}
 
+// add all the middleware here 
 const middleware = [thunk]
 
+// create store 
 const store = createStore(reducers,initialState,composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store
