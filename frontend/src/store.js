88934c6113375b75a56e5fd7890339a86c6dee80
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-01-25 23:24:34 +0530

    Product list's, constant,reducer,action Created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,22 @@
-import {createStore,combineReducers,applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
 // to combine all the middle wares here
-const reducers = combineReducers({})
+const reducers = combineReducers({ productList: productListReducer })
 
-// you can add any data for initialState 
+// you can add any data for initialState
 const initialState = {}
 
-// add all the middleware here 
+// add all the middleware here
 const middleware = [thunk]
 
-// create store 
-const store = createStore(reducers,initialState,composeWithDevTools(applyMiddleware(...middleware)))
+// create store
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store
