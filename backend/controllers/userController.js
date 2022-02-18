90274fa77c4515280b013df35f5700b6e09a3951
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-02-18 13:18:31 +0530

    user registration(creating new user document in mongoDB) and password encryption before adding password to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,6 +22,36 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+  
+  if(user){
+    res.status(401).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  }else{
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
 //@desc   GET user Profile
 //@route  GET /api/user/profile
 //@access Private
@@ -39,4 +69,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
-export { authUser, getUserProfile }
+export { registerUser, authUser, getUserProfile }
