ab66ecb4588cbbf822f4f8f082f1ead8dddca795
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-03-05 21:55:48 +0530

    User Login Reducer, Actions,Constants & updating store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,21 +6,30 @@ import {
   productListReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducers } from './reducers/userReducers'
 
 // to combine all the middle wares here
 const reducers = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducers,
 })
 
 // accessing cartitem from local storage
 const cartItemsFromStorage = localStorage.getItem('cartItem')
   ? JSON.parse(localStorage.getItem('cartItem'))
   : []
+
+// accessing userInfo from local storage
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 // you can add any data for initialState
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 // add all the middleware here
