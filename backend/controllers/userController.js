914a423a40c03db26134fe9affdd538758422869
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-03-08 12:32:30 +0530

    Update Profile Endpoint(backend)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,8 +37,8 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
   })
-  
-  if(user){
+
+  if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -46,7 +46,7 @@ const registerUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
     })
-  }else{
+  } else {
     res.status(400)
     throw new Error('Invalid user data')
   }
@@ -69,4 +69,35 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
-export { registerUser, authUser, getUserProfile }
+
+//@desc   PUT user profile
+//@route  PUT /api/users/profile
+//@access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { registerUser, authUser, getUserProfile, updateUserProfile }
