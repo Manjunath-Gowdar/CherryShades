d173a6a45de98c5778781bb252f105594f3248b0
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-03-19 14:01:17 +0530

    Payment screen,app.js,constant,action,reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from './Screens/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen'
 import ProfileScreen from './Screens/ProfileScreen'
 import ShippingScreen from './Screens/ShippingScreen'
+import PaymentScreen from './Screens/PaymentScreen'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <main className='py-3'>
           <Container>
             <Route path='/shipping' component={ShippingScreen} />
+            <Route path='/payment' component={PaymentScreen} />
             <Route path='/register' component={RegisterScreen} />
             <Route path='/login' component={LoginScreen} />
             <Route path='/profile' component={ProfileScreen} />
