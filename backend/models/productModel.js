05d73c378fd16521c4d0893e4bf19d92020ca045
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-03-23 18:22:25 +0530

    Get Order by ID Endpoint(backend), & previous error handled in productModel.js, userModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,67 +1,74 @@
 import mongoose from 'mongoose'
 
-const reviewSchema = mongoose.Schema({
-  name:{type:String,required:true},
-  rating:{type:Number,required:true},
-  comment:{type:String,required:true},
-},{
-  timestamp:true,
-})
-
-const productSchema = mongoose.Schema(
+const reviewSchema = mongoose.Schema(
   {
-    user:{
-      type:mongoose.Schema.Types.ObjectId,
-      required:true,
-      ref:'User'
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
-    name:{
-      type:String,
-      required:true,
-    },
-    image:{
-      type:String,
-      required:true 
-    },
-    brand:{
-      type:String,
-      required:true
-    },
-    category:{
-      type:String,
-      required:true,
-    },
-    description:{
-      type:String,
-      required:true
-    },
-      reviews:[reviewSchema],
-      rating:{
-        type:Number,
-        required:true,
-        default:0
-      },
-      numReviews:{
-        type:Number,
-        required:true,
-        default:0
-      },
-      price:{
-        type:Number,
-        required:true,
-        default:0
-      },
-      countInStock:{
-        type:Number,
-        required:true,
-        default:0
-      }
-    
   },
   {
-    timestamp: true,
+    timestamps: true,
   }
 )
 
-const Product = mongoose.model('Product',productSchema)
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
 export default Product
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -23,19 +23,19 @@ const userSchema = mongoose.Schema(
     },
   },
   {
-    timestamp: true,
+    timestamps: true,
   }
 )
 
-userSchema.methods.matchPassword = async function(enteredPassword){
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
-userSchema.pre('save',async function(next){
-  if(!this.isModified('password')){
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
     next()
   }
   const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password,salt)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 const User = mongoose.model('User', userSchema)
