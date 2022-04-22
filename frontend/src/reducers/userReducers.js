767200cf8bfd0f5a6745450ad2e217f1acf7095a
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-04-22 15:57:13 +0530

    User Details RESET, & Order RESET

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,7 @@
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -51,22 +52,24 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       return { loading: false, user: action.payload }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
+    case USER_DETAILS_RESET:
+      return { user: {} }
     default:
       return state
   }
 }
 
-export const userUpdateProfileReducer = (state={},action)=>{
-  switch(action.type){
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
     case USER_UPDATE_PROFILE_REQUEST:
-      return {loading:true}
+      return { loading: true }
     case USER_UPDATE_PROFILE_SUCCESS:
-      return {loading:false,success:true,userInfo:action.payload}
+      return { loading: false, success: true, userInfo: action.payload }
     case USER_UPDATE_PROFILE_FAIL:
-      return {loading:false,error:action.payload}
+      return { loading: false, error: action.payload }
     case USER_UPDATE_PROFILE_RESET:
       return {}
-      default:
+    default:
       return state
   }
 }
