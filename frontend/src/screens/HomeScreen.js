b658072de3c0e04f8377801af851d6c2fa6746de
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-06-29 12:49:39 +0530

    Fixed - Image and card consistent in size @ HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -27,7 +27,8 @@ const HomeScreen = () => {
       ) : (
         <Row>
           {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Col className='align-items-stretch d-flex'
+            key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
           ))}
