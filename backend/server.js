dea082a5e27b1e34d738e2e1b16d49b5dc8b4aed
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-07-06 21:23:54 +0530

    Morgan - setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import morgan from 'morgan'
 import productRoute from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import userRoute from './routes/userRoutes.js'
@@ -14,14 +15,19 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json()) //to parse data in req.body
 
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-// to send paypal config 
-app.get('/api/config/paypal',(req,res)=>{
+// to send paypal config
+app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
@@ -30,7 +36,7 @@ app.use('/api/users', userRoute)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-// uploading image 
+// uploading image
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
